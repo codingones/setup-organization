9ba69b0f4b9fc03f4c6d7456947a8f5c8521130e
Romain Cambonie <romain.cambonie@gmail.com>
2023-04-05 13:55:13 +0200

    feat: execute workflow on create and reactivate workflow autodelete

--- organization.github.tf
+++ organization.github.tf
@@ -13,3 +13,9 @@ resource "github_actions_organization_secret" "tf_api_token" {
   plaintext_value = tfe_team_token.team_token.token
   visibility      = "all"
 }
+
+resource "github_actions_organization_variable" "tf_owners_team_id" {
+  variable_name = "TFE_OWNERS_TEAM_ID"
+  value         = data.tfe_team.owners.id
+  visibility    = "all"
+}
--- organization.tfe.tf
+++ organization.tfe.tf
@@ -12,25 +12,25 @@ resource "tfe_team_token" "team_token" {
   team_id = data.tfe_team.owners.id
 }
 
-resource "tfe_workspace" "organization_workspace" {
-  name         = "organization"
-  organization = tfe_organization.organization.name
-}
-
-resource "tfe_variable" "var_organization_name" {
-  key          = "organization_name"
-  value        = tfe_organization.organization.name
-  category     = "terraform"
-  workspace_id = tfe_workspace.organization_workspace.id
-  description  = "organization name on github and terraform cloud"
-}
-
-resource "tfe_variable" "tfe_team_token" {
-  key          = "tfe_team_token"
-  value        = tfe_team_token.team_token.token
-  category     = "env"
-  workspace_id = tfe_workspace.organization_workspace.id
-  description  = "organization name on github and terraform cloud"
-  sensitive    = true
-}
+#resource "tfe_workspace" "organization_workspace" {
+#  name         = "organization"
+#  organization = tfe_organization.organization.name
+#}
+#
+#resource "tfe_variable" "var_organization_name" {
+#  key          = "organization_name"
+#  value        = tfe_organization.organization.name
+#  category     = "terraform"
+#  workspace_id = tfe_workspace.organization_workspace.id
+#  description  = "organization name on github and terraform cloud"
+#}
+#
+#resource "tfe_variable" "tfe_team_token" {
+#  key          = "tfe_team_token"
+#  value        = tfe_team_token.team_token.token
+#  category     = "env"
+#  workspace_id = tfe_workspace.organization_workspace.id
+#  description  = "organization name on github and terraform cloud"
+#  sensitive    = true
+#}
 
